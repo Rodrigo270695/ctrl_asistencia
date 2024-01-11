12d0300421120f29f0a2e84b9b12dc3f556ffe48
Rodrigo270695 <70205349+Rodrigo270695@users.noreply.github.com>
2024-01-11 15:29:01 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
+/* export default defineConfig({
     plugins: [
         laravel({
             input: 'resources/js/app.js',
@@ -13,6 +13,27 @@ export default defineConfig({
                 transformAssetUrls: {
                     base: null,
                     includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+});
+ */
+
+export default defineConfig({
+    server: {
+        https: true,
+    },
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: '/',
+                    includeAbsolute: true,
                 },
             },
         }),
