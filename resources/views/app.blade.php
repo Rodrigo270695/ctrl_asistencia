8da435ac8057c2b77e4e643a8789d678a2e17080
Rodrigo270695 <70205349+Rodrigo270695@users.noreply.github.com>
2024-01-26 12:04:19 -0500

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="Content-Security-Policy" content="block-all-mixed-content">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Macga') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
